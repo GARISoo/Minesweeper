ba09496e0f9258114279680d55a868bcd424fae1
Haralds Treilons <haralds_treilons@inbox.lv>
2022-11-12 18:42:23 +0200

    added global context

--- src/components/Minesweepers/Minesweepers.tsx
+++ src/components/Minesweepers/Minesweepers.tsx
@@ -1,12 +1,17 @@
+/* eslint-disable react/no-array-index-key */
 import { useState, useRef, useEffect } from 'react';
 import autoSolve from '../../helpers/autoSolver';
+import useGame from '../../hooks/useGame';
+import useGameContext from '../../hooks/useGameContext';
 import { useHasChanged } from '../../hooks/usePrevious';
 import socket from '../../socket';
 import styles from './Minesweepers.module.scss';
 
 const Minesweepers = () => {
-  const [field, setField] = useState<string[][]>([]);
-  const [flaggedCells, setFlaggedCells] = useState<string[]>([]);
+  const { field, flaggedCells } = useGameContext();
+  const {
+    startNewLevel, openCell, openManyCells, flagCell, flagManyCells, getSrcPath,
+  } = useGame();
   const [cellsToOpen, setCellsToOpen] = useState<string[]>([]);
   const [cellsToFlag, setCellsToFlag] = useState<string[]>([]);
   const [autoSolving, setAutoSolving] = useState(false);
@@ -15,139 +20,33 @@ const Minesweepers = () => {
 
   const ws = useRef<any>(null);
 
-  const newLevel = (level: number) => {
-    socket.send(`new ${level}`);
-    setFlaggedCells([]);
-  };
-
-  const flagCellAsBomb = (x: number, y: number, value: string) => {
-    const cell = `${x} ${y}`;
-
-    if (value === '□') {
-      if (flaggedCells.includes(cell)) {
-        setFlaggedCells(flaggedCells.filter((el) => el !== cell));
-      } else {
-        setFlaggedCells([...flaggedCells, cell]);
-      }
-    }
-  };
-
-  const flagManyCellsAsBomb = (cells: string[]) => {
-    setFlaggedCells([...flaggedCells, ...cells]);
-  };
-
   const levels = [
     {
       id: 1,
       name: 'Beginner',
-      handleNewLevel: () => newLevel(1),
+      handleNewLevel: () => startNewLevel(1),
     },
     {
       id: 2,
       name: 'Medium',
-      handleNewLevel: () => newLevel(2),
+      handleNewLevel: () => startNewLevel(2),
     },
     {
       id: 3,
       name: 'Advanced',
-      handleNewLevel: () => newLevel(3),
+      handleNewLevel: () => startNewLevel(3),
     },
     {
       id: 4,
       name: 'Hardcore',
-      handleNewLevel: () => newLevel(4),
+      handleNewLevel: () => startNewLevel(4),
     },
   ];
-
-  const openCell = (x: number, y: number) => {
-    socket.send(`open ${x} ${y}`);
-  };
-
-  useEffect(() => {
-    socket.onopen = () => {
-      socket.send('new 1');
-    };
-
-    socket.onclose = () => {
-      console.log('closed');
-    };
-
-    socket.onmessage = ({ data }) => {
-      const isNewMap = data.includes('map');
-
-      if (isNewMap) {
-        const adjustedField = data.split('\n').slice(1, -1).map((cell: any) => cell.split(''));
-        setField(adjustedField);
-      } else {
-        socket.send('map');
-      }
-    };
-
-    ws.current = socket;
-  }, []);
-
-  const handleOpenCell = (x: number, y: number) => {
-    const checkFlaggedCell = flaggedCells.includes(`${x} ${y}`);
-
-    if (!checkFlaggedCell) {
-      openCell(x, y);
-    }
-  };
-
-  const determineSVG = (cell: string, x: number, y: number) => {
-    let path = 'assets/mines/';
-
-    switch (cell) {
-      case '0':
-        path += '0.svg';
-        break;
-      case '1':
-        path += '1.svg';
-        break;
-      case '2':
-        path += '2.svg';
-        break;
-      case '3':
-        path += '3.svg';
-        break;
-      case '4':
-        path += '4.svg';
-        break;
-      case '5':
-        path += '5.svg';
-        break;
-      case '6':
-        path += '6.svg';
-        break;
-      case '7':
-        path += '7.svg';
-        break;
-      case '8':
-        path += '8.svg';
-        break;
-      case '□':
-        path += 'empty.svg';
-        break;
-      case '*':
-        path += 'mine.svg';
-        break;
-      default:
-        break;
-    }
-
-    const checkFlaggedCell = flaggedCells.includes(`${x} ${y}`);
-
-    if (checkFlaggedCell) {
-      path = 'assets/mines/flag.svg';
-    }
-
-    return path;
-  };
 
   const handleRightClick = (e: any, x: number, y: number, value: string) => {
     e.preventDefault();
 
-    flagCellAsBomb(x, y, value);
+    flagCell(x, y, value);
   };
 
   const tryToSolve = () => {
@@ -169,25 +68,16 @@ const Minesweepers = () => {
     console.log('cellsToOpen:', cellsToOpen, 'cellsToFlag:', cellsToFlag);
   };
 
-  const manyCellsToOpen = (cells: string[]) => {
-    if (cells.length) {
-      cells.forEach((cell) => {
-        const [x, y] = cell.split(' ');
-        openCell(Number(x), Number(y));
-      });
-    }
-  };
-
   // eslint-disable-next-line consistent-return
-  useEffect(() => {
-    flagManyCellsAsBomb(cellsToFlag);
-    manyCellsToOpen(cellsToOpen);
+  // useEffect(() => {
+  //   flagManyCellsAsBomb(cellsToFlag);
+  //   manyCellsToOpen(cellsToOpen);
 
-    if (autoSolving && (cellsToOpen.length || cellsToFlag.length)) {
-      const interval = setInterval(tryToSolve, 1000);
-      return () => clearInterval(interval);
-    }
-  }, [cellsToFlag, cellsToOpen, autoSolving]);
+  //   if (autoSolving && (cellsToOpen.length || cellsToFlag.length)) {
+  //     const interval = setInterval(tryToSolve, 1000);
+  //     return () => clearInterval(interval);
+  //   }
+  // }, [cellsToFlag, cellsToOpen, autoSolving]);
 
   // if something doesnt work, remove this useffect
   useEffect(() => {
@@ -214,15 +104,15 @@ const Minesweepers = () => {
       </div>
       <div>
         {field.map((cellsRow, y) => (
-          <div key={Math.random() * 15785} className={styles.fieldRow}>
+          <div key={Math.random() * 12345} className={styles.fieldRow}>
             {cellsRow.map((cell, x) => (
               <span
                 className={styles.fieldCell}
-                onClick={() => handleOpenCell(x, y)}
-                key={Math.random() * 15785}
+                onClick={() => openCell(x, y)}
+                key={`${y} ${x}`}
                 onContextMenu={(e) => handleRightClick(e, x, y, cell)}
               >
-                <img src={determineSVG(cell, x, y)} alt="cell" className={styles.fieldCellSVG} />
+                <img src={getSrcPath(cell, x, y)} alt="cell" className={styles.fieldCellSVG} />
               </span>
             ))}
           </div>
